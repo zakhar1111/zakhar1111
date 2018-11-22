07458a5f398ca21101a681fadfa48b8b3ed30c5d
zakhar1111 <40389817+zakhar1111@users.noreply.github.com>
2018-11-22 18:21:18 +0300

    Add files via upload

---
+++ v.3.ps1
@@ -0,0 +1,21 @@
+﻿$SiteFolderPath = "C:\inetpub\wwwroot\3"                                      # Website Folder
+$SiteAppPool = "MyAppPool"                                                    # Application Pool Name
+$SiteName = "MySite"                                                          # IIS Site Name
+$SiteHostName = "my-3-page.test"                                              # Host Header - address
+
+if (! (Test-Path -path $SiteFolderPath))
+{
+    New-Item $SiteFolderPath -type Directory
+    Set-Content $SiteFolderPath\Default.htm "<h1>Hello IIS</h1>"
+}
+
+if(!(Test-Path -path IIS:\AppPools\$SiteAppPool))
+{
+    New-Item IIS:\AppPools\$SiteAppPool #-force
+}
+
+if(!(Test-Path -path IIS:\Sites\$SiteName))
+{ 
+    New-Item IIS:\Sites\$SiteName  -bindings @{protocol="http";bindingInformation=":80:"+$SiteHostName} -physicalPath $SiteFolderPath
+    Set-ItemProperty IIS:\Sites\$SiteName -name applicationPool -value $SiteAppPool
+}
